738dca5f337329b1d04634d81bf617f064af2457
Vu <webdevqn@gmail.com>
2015-11-21 20:49:37 +0700

    Initialize api v1 user

---
+++ application/controllers/api/v1/User.php
@@ -0,0 +1,61 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    public function index() {
+        
+    }
+
+    public function user_list() {
+        $output = array(
+            'code' => 200,
+            'status' => "SUCCESS",
+            'message' => 'API works well',
+            'data' => array(
+                array(
+                    'id' => 113,
+                    'user_name' => 'Vu',
+                    'email' => 'vu@thodaico.vn',
+                    'status' => 'active',
+                    'role' => 'admin',
+                    'created_date' => 1231341241,
+                    'updated_date' => 1231341241,
+                    'last_activity' => 352342342,
+                    'created_by' => 113
+                ),
+                array(
+                    'id' => 114,
+                    'user_name' => 'Bao',
+                    'email' => 'bao@thodaico.vn',
+                    'status' => 'active',
+                    'role' => 'admin',
+                    'created_date' => 1231341241,
+                    'updated_date' => 1231341241,
+                    'last_activity' => 352342342,
+                    'created_by' => 113
+                ),
+                array(
+                    'id' => 115,
+                    'user_name' => 'Da',
+                    'email' => 'da@thodaico.vn',
+                    'status' => 'active',
+                    'role' => 'admin',
+                    'created_date' => 1231341241,
+                    'updated_date' => 1231341241,
+                    'last_activity' => 352342342,
+                    'created_by' => 113
+                )
+            )
+        );
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output));
+    }
+    
+    public function user_check() {
+        $user_name = "";                
+    }
+
+}
