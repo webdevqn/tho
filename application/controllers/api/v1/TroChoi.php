fb4842d311598fd10853cd2997c78d570f14f514
Vu <webdevqn@gmail.com>
2015-12-14 08:08:07 +0700

    Up

---
+++ application/controllers/api/v1/TroChoi.php
@@ -0,0 +1,496 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Product_model');
+        $this->load->helper('my_input');
+        define("_TRO_NAY_HAY_NE_", "Hello world.");
+        define("_TRO_NAY_DO_OM_", "Hello world.");
+        define("_BUON_NGU_QUA_", "Hello world.");
+        define("_DI_NGU_THOI_", "Hello world.");
+    }
+
+    public function index()
+    {
+        echo 23423;
+    }
+
+    //CATEGORIES
+    public function category_list()
+    {
+        $response = $this->Product_model->category_list();
+        if ($response != 400)
+        {
+            $output = array(
+                'code' => 200,
+                'status' => 'SUCCESS',
+                'message' => 'List of categories',
+                'data' => $this->Product_model->category_list()
+            );
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Failed to get category list'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function category_add()
+    {
+
+        $input = array(
+            'name' => $this->security->xss_clean(trim($this->input->post('name'))),
+            'slug' => create_slug($this->security->xss_clean(trim($this->input->post('name')))),
+            'status' => 1,
+            'token' => create_token(),
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => time(),
+            'updated_at' => time()
+        );
+        if (check_input_element_valid($input))
+        {
+            $request = $this->Product_model->category_add($input);
+            if ($request !== '400')
+            {
+                $request1 = $this->Product_model->category_detail($request);
+                if ($request1 !== '400')
+                {
+                    $output = array(
+                        'code' => '200',
+                        'status' => 'SUCCESS',
+                        'message' => 'Add category successfully',
+                        'data' => $request1
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'FAILED',
+                        'message' => 'Add category failed',
+                        'data' => $input
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Server Error',
+                    'data' => array()
+                );
+            }
+        }
+        else
+        {
+            $request = 400;
+            $output = array(
+                'code' => $request,
+                'status' => 'FAILED',
+                'message' => 'Missing parameters'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function category_detail()
+    {
+        $id = $this->security->xss_clean(trim($this->input->post('id')));
+        if (isset($id) && !is_null($id) && is_numeric($id) && intval($id) > 0)
+        {
+            $request = $this->Product_model->category_check($id);
+            if ($request == 200)
+            {
+                $request1 = $this->Product_model->category_detail($id);
+                if ($request1 !== '400')
+                {
+                    $output = array(
+                        'code' => '200',
+                        'status' => 'SUCCESS',
+                        'message' => 'Category detail',
+                        'data' => $this->Product_model->category_detail($id)
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => '400',
+                        'status' => 'FAILED',
+                        'message' => 'Failed to get category detail',
+                        'data' => array('id' => $id)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Category ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Role ID is not in right format'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function category_delete()
+    {
+        $id = $this->input->post('id');
+        if (isset($id) && !is_null($id) && is_numeric($id) && intval($id) > 0)
+        {
+            $request = $this->Product_model->category_check($id);
+            if ($request == 200)
+            {
+                $request1 = $this->Product_model->category_delete($id);
+                if ($request1 == 200)
+                {
+                    $output = array(
+                        'code' => $request1,
+                        'status' => 'SUCCESS',
+                        'message' => 'Delete category successfully',
+                        'data' => array('id' => $id)
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request1,
+                        'status' => 'FAILED',
+                        'message' => 'Failed to delete category',
+                        'data' => array('id' => $id)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Category ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Category ID is not in right format'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function giaiTriNgayChuNhat()
+    {
+        $troChoi = $this->security->xss_clean(trim($this->input->post('troChoi')));
+        $thongTinTroChoi = array(
+            'tenTroChoi' => $this->security->xss_clean(trim($this->input->post('tenTroChoi'))),
+            'kiHieuTroChoi' => taoKiHieu($this->security->xss_clean(trim($this->input->post('tenTroChoi'))))
+        );
+        if (kiemTraTroChoi($thongTinTroChoi))
+        {
+            $ketQua = $this->troChoi->chonTroChoi($troChoi);
+            if ($ketQua === _TRO_NAY_HAY_NE_)
+            {
+                $ketQua1 = $this->troChoi->chayTroChoi($troChoi, $thongTinTroChoi);
+                if ($ketQua1 === _TRO_NAY_DO_OM_)
+                {
+                    $ketQua2 = $this->troChoi->tatTroChoi($troChoi);
+                    if ($ketQua2 !== _BUON_NGU_QUA_)
+                    {
+                        $bayGioTinhSao = array(
+                            'tinhTrang' => _DI_NGU_THOI_
+                        );
+                    }
+                    else
+                    {
+                        $output = array(
+                            'code' => $request,
+                            'status' => 'FAILED',
+                            'message' => 'SERVER ERROR'
+                        );
+                    }
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'FAILED',
+                        'message' => 'Failed to update category',
+                        'data' => array('title' => $title)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Category ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $request = 400;
+            $output = array(
+                'code' => $request,
+                'status' => 'FAILED',
+                'message' => 'Missing parameters'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    //PRODUCTS
+    public function product_list()
+    {
+        $response = $this->Product_model->product_list();
+        if ($response != 400)
+        {
+            $output = array(
+                'code' => 200,
+                'status' => 'SUCCESS',
+                'message' => 'List of products',
+                'data' => $this->Product_model->product_list()
+            );
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Failed to get product list'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function product_add()
+    {
+        $input = array();
+        if (isset($title) && !is_null($title))
+        {
+            $request = $this->Product_model->product_add($title);
+            if ($request == 200)
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'SUCCESS',
+                    'message' => 'Add product successfully',
+                    'data' => array('title' => $title)
+                );
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Server Error',
+                    'data' => array('title' => $title)
+                );
+            }
+        }
+        else
+        {
+            $request = 400;
+            $output = array(
+                'code' => $request,
+                'status' => 'FAILED',
+                'message' => 'Missing parameters'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function product_detail()
+    {
+        $id = $this->input->post('id');
+        if (isset($id) && !is_null($id) && is_numeric($id) && intval($id) > 0)
+        {
+            $request = $this->Product_model->product_check($id);
+            if ($request == 200)
+            {
+                $request1 = $this->Product_model->product_detail($id);
+                if ($request1 != 400)
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'SUCCESS',
+                        'message' => 'Product detail',
+                        'data' => $this->Product_model->product_detail($id)
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'FAILED',
+                        'message' => 'Failed to get product detail',
+                        'data' => array('id' => $id)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Product ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Product ID is not in right format'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function product_delete()
+    {
+        $id = $this->input->post('id');
+        if (isset($id) && !is_null($id) && is_numeric($id) && intval($id) > 0)
+        {
+            $request = $this->Product_model->product_check($id);
+            if ($request == 200)
+            {
+                $request1 = $this->Product_model->product_delete($id);
+                if ($request1 == 200)
+                {
+                    $output = array(
+                        'code' => $request1,
+                        'status' => 'SUCCESS',
+                        'message' => 'Delete product successfully',
+                        'data' => array('id' => $id)
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request1,
+                        'status' => 'FAILED',
+                        'message' => 'Failed to delete product',
+                        'data' => array('id' => $id)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Product ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $output = array(
+                'code' => 400,
+                'status' => 'FAILED',
+                'message' => 'Product ID is not in right format'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+    public function product_edit()
+    {
+        $id = $this->input->post('id');
+        $title = $this->input->post('title');
+        if (isset($id) && isset($title) && is_numeric($id) && intval($id) > 0)
+        {
+            $request = $this->Product_model->product_check($id);
+            if ($request == 200)
+            {
+                $request1 = $this->Product_model->product_edit($id, $title);
+                if ($request1 == 200)
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'SUCCESS',
+                        'message' => 'Update product successfully',
+                        'data' => array(
+                            'id' => $id,
+                            'title' => $title
+                        )
+                    );
+                }
+                else
+                {
+                    $output = array(
+                        'code' => $request,
+                        'status' => 'FAILED',
+                        'message' => 'Failed to update product',
+                        'data' => array('title' => $title)
+                    );
+                }
+            }
+            else
+            {
+                $output = array(
+                    'code' => $request,
+                    'status' => 'FAILED',
+                    'message' => 'Product ID did not exist'
+                );
+            }
+        }
+        else
+        {
+            $request = 400;
+            $output = array(
+                'code' => $request,
+                'status' => 'FAILED',
+                'message' => 'Missing parameters'
+            );
+        }
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($output, JSON_NUMERIC_CHECK));
+    }
+
+}
